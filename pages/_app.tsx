d6aea321411636a374741de0f6e718282a0545e0
Jake Schaffer <imjakedev@gmail.com>
2021-05-21 16:55:39 -0500

    updated _app.tsx with react query boiler plate code

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,24 @@
 import '../styles/globals.css'
+import React from 'react'
 import type { AppProps } from 'next/app'
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+import { QueryClient, QueryClientProvider } from 'react-query'
+import { Hydrate } from 'react-query/hydration'
+
+export default function MyApp({ Component, pageProps }: AppProps) {
+  // not sure if any is the best type to use here
+  const queryClientRef: any = React.useRef()
+  console.log(queryClientRef);
+
+  if (!queryClientRef.current) {
+    queryClientRef.current = new QueryClient()
+  }
+
+  return (
+    <QueryClientProvider client={queryClientRef.current}>
+      <Hydrate state={pageProps.dehydratedState}>
+        <Component {...pageProps} />
+      </Hydrate>
+    </QueryClientProvider>
+  )
 }
-export default MyApp
