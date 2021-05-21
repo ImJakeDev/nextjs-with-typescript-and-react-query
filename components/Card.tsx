296bfb03554cdce3f7a1f6b1dbc84c161345f71f
Jake Schaffer <imjakedev@gmail.com>
2021-05-21 16:12:08 -0500

    refactored Card component with an interface

--- components/Card.tsx
+++ components/Card.tsx
@@ -1,9 +1,15 @@
 import styles from '../styles/Home.module.css'
 
-export default function Card(props: {link:string, title:string, description:string}) {
+interface Props {
+  link:string;
+  title:string;
+  description:string;
+}
+
+export default function Card(props: Props) {
   const {link, title, description} = props
   return (
-    <a href={link} className={styles.card}>
+    <a href={link} target="_blank" className={styles.card}>
       <h2>{title} &rarr;</h2>
       <p>{description}</p>
     </a>
